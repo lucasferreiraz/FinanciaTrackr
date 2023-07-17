895e0d1b755fa08ff50b96b5e755ac2883ac22c9
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-07-16 23:36:43 -0300

    chore: initialized spring boot project

---
+++ backend/src/main/java/com/enterprise/api/financiatracker/FinanciatrackerApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.api.financiatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanciatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciatrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enterprise/api/financiatracker/FinanciatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.api.financiatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
