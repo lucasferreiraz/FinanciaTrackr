62fabdf7c33a60bf876d2e707c0b37f43a702695
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-07-17 22:39:10 -0300

    feat: create and find especific categories

--- backend/src/main/java/com/enterprise/api/financiatrackr/controllers/CategoryController.java
+++ backend/src/main/java/com/enterprise/api/financiatrackr/controllers/CategoryController.java
@@ -1,14 +1,22 @@
 package com.enterprise.api.financiatrackr.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.enterprise.api.financiatrackr.entities.Category;
 import com.enterprise.api.financiatrackr.repositories.CategoryRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categories")
@@ -16,9 +24,27 @@ public class CategoryController {
 
     @Autowired
     private CategoryRepository categoryRepository;
-    
+
     @GetMapping
-    public List<Category> getAll() { 
+    public List<Category> getAll() {
         return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+        Category savedCategory = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(savedCategory);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        return (category != null) ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
     }
 }
