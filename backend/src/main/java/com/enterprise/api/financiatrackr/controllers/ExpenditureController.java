1406c020c03c36229e42a0eda0f45bf116c0327c
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-07-21 21:23:04 -0300

    feat: delete expenditure

--- backend/src/main/java/com/enterprise/api/financiatrackr/controllers/ExpenditureController.java
+++ backend/src/main/java/com/enterprise/api/financiatrackr/controllers/ExpenditureController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,11 @@ public class ExpenditureController {
         Expenditure savedExpenditure = expenditureService.save(expenditure);
         publisher.publishEvent(new CreatedResourceEvent(this, response, savedExpenditure.getId()));
         return ResponseEntity.status(HttpStatus.CREATED).body(savedExpenditure);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        expenditureRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
