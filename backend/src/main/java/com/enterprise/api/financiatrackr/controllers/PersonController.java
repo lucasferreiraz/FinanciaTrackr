1a83f39e581fa31a58d53558eb60c882f130d33a
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-08-01 19:48:12 -0300

    feat: dinamic and pageable search query for persons

--- backend/src/main/java/com/enterprise/api/financiatrackr/controllers/PersonController.java
+++ backend/src/main/java/com/enterprise/api/financiatrackr/controllers/PersonController.java
@@ -1,10 +1,11 @@
 package com.enterprise.api.financiatrackr.controllers;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.enterprise.api.financiatrackr.entities.Person;
@@ -38,8 +40,11 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping
-    public List<Person> getAll() {
-        return personRepository.findAll();
+    public Page<Person> getAll(
+        @RequestParam(value = "name", defaultValue = "", required = false) String name,
+        Pageable pageable
+    ) {
+        return personRepository.findByNameContaining(name, pageable);
     }
 
     @PostMapping
--- backend/src/main/java/com/enterprise/api/financiatrackr/repositories/PersonRepository.java
+++ backend/src/main/java/com/enterprise/api/financiatrackr/repositories/PersonRepository.java
@@ -1,9 +1,13 @@
 package com.enterprise.api.financiatrackr.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.enterprise.api.financiatrackr.entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     
+    Page<Person> findByNameContaining(String name, Pageable pageable);
+
 }
