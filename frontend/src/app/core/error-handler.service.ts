277170900cab942b7dc7fa68a4dc636aab818cf4
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-08-02 14:54:42 -0300

    feat: obtendo mensagem de erro da resposta

--- frontend/src/app/core/error-handler.service.ts
+++ frontend/src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -15,6 +16,21 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (
+      errorResponse instanceof HttpErrorResponse &&
+      errorResponse.status >= 400 &&
+      errorResponse.status <= 499
+    ) {
+
+      msg = 'Ocorreu um erro ao processar a sua solicitação.';
+
+      try {
+        msg = errorResponse.error.userMessage;
+      } catch (e) {
+      }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
