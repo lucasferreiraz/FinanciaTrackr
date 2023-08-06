0cacf7adb33eabe8f0d7ede2e5f3c2fcdc95d800
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-08-05 21:33:05 -0300

    refact: routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada/pagina-nao-encontrada.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  { path: 'lancamentos/:id', component: LancamentoCadastroComponent},
+  { path: 'pessoas', component: PessoasPesquisaComponent},
+  { path: 'pessoas/nova', component: PessoaCadastroComponent},
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
+  { path: '**', redirectTo: 'pagina-nao-encontrada'}
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,37 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PrimeNGConfig } from 'primeng/api';
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-import { RouterModule, Routes } from '@angular/router';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { PrimeNGConfig } from 'primeng/api';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 registerLocaleData(localePt);
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  { path: 'lancamentos/:id', component: LancamentoCadastroComponent},
-  { path: 'pessoas', component: PessoasPesquisaComponent},
-  { path: 'pessoas/nova', component: PessoaCadastroComponent},
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  { path: '**', redirectTo: 'pagina-nao-encontrada'}
-]
 
 @NgModule({
   declarations: [
@@ -45,7 +28,7 @@ const routes: Routes = [
     CoreModule,
     LancamentosModule,
     PessoasModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [
     PrimeNGConfig,
