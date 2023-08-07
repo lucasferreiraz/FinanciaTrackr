14607c8c76df4bd20b8c23866e5af80feb645a88
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-08-07 15:29:19 -0300

    feat: exibindo nome do usuario logado

--- frontend/src/app/core/navbar/navbar.component.ts
+++ frontend/src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/seguranca/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  usuarioLogado: string = ''
   exibindoMenu: boolean = true
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.usuarioLogado = this.auth.jwtPayload.name
+  }
+
+
 }
