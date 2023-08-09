f069acfdaa86f78865d8eb5c6370b84b2ec1c1b7
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-08-09 12:33:30 -0300

    feat: tratando usuarios deslogados

--- frontend/src/app/seguranca/auth.guard.ts
+++ frontend/src/app/seguranca/auth.guard.ts
@@ -6,6 +6,19 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService)
   const router = inject(Router)
 
+  if (authService.isAccessTokenInvalido()) {
+    console.log('Navagação com access token inválido.');
+
+    authService.obterNovoAccessToken()
+      .subscribe();
+
+    if (authService.isAccessTokenInvalido()) {
+      router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  } else
+
   if (route.data?.['roles'] && !authService.temQualquerPermissao(route.data?.['roles'])) {
     router.navigate(['/nao-autorizado']);
     return false;
